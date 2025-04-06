2154fb781b51b4e39cb01eb2b7f57f13e605a762
Aziz <141174883+AzizDevCraft@users.noreply.github.com>
2025-04-06 19:08:30 +0200

    une petite rectification pour etre plus général

--- scraping/scraper.py
+++ scraping/scraper.py
@@ -20,14 +20,14 @@ with open ('data/raw/annonces-url.json', 'r', encoding= 'utf-8') as file :
     data = json.load (file)
 
 
-for _ in range (10001, len (data)) : 
+for _ in range (0, len (data)) : 
     annonce = extract_data (data [_]["url_annonce"], headers)
-    print (f"annonce {_} cb scrape !")
+    print (f"annonce {_ + 1} cb scrape !")
     with open ('data/raw/annonces-data.ndjson', 'a', encoding='utf-8') as file : 
         file.write (json.dumps (annonce, indent=4) + '\n')
-    print (f"-------------------- annonce {_} ajoutés ---------------------")
+    print (f"-------------------- annonce {_ + 1} ajoutés ---------------------")
     if _%500 == 0 : 
         time.sleep (5)
-        print (f'une petite pause on a atteind {_} annonces !')
+        print (f'une petite pause on a atteind {_ + 1} annonces !')
 fin = time.time () - debut
 print (f'temps ecoule : {fin/3600} heures')
